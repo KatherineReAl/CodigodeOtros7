ba1b61c4e2b0fe60355d2b450c9149606a042cae
Katherine RA <kathephone1805@gmail.com>
2025-01-30 13:20:16 -0600

    se agregarn comentarios de las correcciones del ejercicio de códigos de otros 7

--- Codigo7.java
+++ Codigo7.java
@@ -1,39 +1,47 @@
-public class Código7 {
+package ejerciciocodigo7;
+/*
+ * Este codigo permite al usuario ingresar el nombre de un país y recibir su capital. El programa continúa ejecutándose hasta que el usuario escriba "salir" y si no se sabe la respuesta guarda lo que le pone el usuario para después tener esa respuesta
+ */
+//importo las bibliotecas
+import java.util.HashMap;
+import java.util.Scanner;
+public class Codigo7 {//le quite el acento
+	public static void main(String[] args) { //se agrega el main
 
-    Scanner s = new Scanner();
+    Scanner s = new Scanner(System.in);//Agrego el system.in
 
-    HashMap<Byte, Double> ca = new HashMap<>();
+    HashMap<String, String> capitales = new HashMap<>(); //cambio los tipos de datos a string
 
     capitales.put("Canadá", "Otawwa");
     capitales.put("Estados Unidos", "Washington DC");
     capitales.put("México", "México DF");
     capitales.put("Belice", "Belmopán");
     capitales.put("Costa rica", "San José");
-    capitales.put("El Salvador");
+    capitales.put("El Salvador","San Salvador"); //se pone capital para cumplir con HashMap
     capitales.put("Guatemala", "Ciudad de Guatemala");
     capitales.put("Honduras", "Tegucigalpa");
     capitales.put("Nicaragua", "Managua");
     capitales.put("Panamá", "Panamá");
    
-    Sting c = "";
+    String c = "";//String bien escrito
 
     do {
-      System.in.print("Escribe el nombre de un país y te diré su capital: ");
-      c = s.nextDouble();
+      System.out.print("Escribe el nombre de un país y te diré su capital: ");
+      c = s.nextLine();//out en el print y nextLine
       
-      if (!c.equal("salir")) {
-        if (ca.containsValue(c)) {
-          System.print("La capital de " + c);
-          System.out.println(" es " + ca.put(c));
+      if (!c.equals("salir")) {//equals
+        if (capitales.containsValue(c)) {//capitales
+          System.out.print("La capital de " + c);//out
+          System.out.println(" es " + capitales.get(c));//get
         } else {
           System.out.print("No conozco la respuesta ");
           System.out.print("¿cuál es la capital de " + c + "?: ");
           String ca = s.nextLine();
-          ca.put(c, ca);
+          capitales.put(c, ca);
           System.out.println("Gracias por enseñarme nuevas capitales");
         }
       }
-    } wile (!c.equals("salir");
+    } while (!c.equals("salir"));//while y el )
   }
 
 }
